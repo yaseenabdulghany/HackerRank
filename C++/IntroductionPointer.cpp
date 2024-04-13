9dcc3d7688b4a8f812257c81726575bf7a1d32a6
Yaseen Abdulghany <62149387+yaseenabdulghany@users.noreply.github.com>
2024-04-13 14:20:26 +0200

    Create IntroductionPointer.cpp

---
+++ C++/IntroductionPointer.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define FAST_DOT_COM  ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define int long long
+const int N = 2e5 + 5 ,MOD=1e9+7, INF=0X3F3F3F3F , MAX= 2e5 + 7;
+void solve(){
+    int n , m ;
+    cin >> n >> m ;
+    cout<<n+m<<"\n"<<abs(n-m);
+}
+int32_t main(){
+    FAST_DOT_COM
+    int t=1  ;
+//    cin>>t ;
+    while (t--) solve();
+    return 0;
+}

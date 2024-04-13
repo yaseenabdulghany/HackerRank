66b72a4b29a55dbb5576e8ebed5a081c71f1be76
Yaseen Abdulghany <62149387+yaseenabdulghany@users.noreply.github.com>
2024-04-13 14:42:28 +0200

    Create Vector-Sort.cpp

---
+++ C++/Vector-Sort.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define FAST_DOT_COM  ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define int long long
+const int N = 2e5 + 5 ,MOD=1e9+7, INF=0X3F3F3F3F , MAX= 1e6;
+void solve(){
+    int n ;
+    cin >> n ;
+    vector<int>v(n);
+    for(auto &i :v )cin>>i;
+    sort(v.begin(),v.end());
+    for(auto i : v)cout<<i<<" ";
+}
+int32_t main(){
+    FAST_DOT_COM
+    int t=1  ;
+//    cin>>t ;
+    while (t--) solve();
+    return 0;
+}
